42cbb6540c2d9fe8a65e100c6a37f85c12ffbcbc
Ferreira Dorian <dorianferreira2@gmail.com>
2021-06-15 16:23:55 +0200

    HTML: Ajout popup

---
+++ html/script.js
@@ -0,0 +1,12 @@
+var btnPopup = document.getElementById('btnPopup');
+var overlay = document.getElementById('overlay');
+btnPopup.addEventListener('click',openMoadl);
+function openMoadl() {
+overlay.style.display='block';
+}
+
+var btnClose = document.getElementById('btnClose');
+btnClose.addEventListener('click',closeModal);
+function closeModal() {
+overlay.style.display='none';
+}
