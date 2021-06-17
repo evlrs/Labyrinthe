289b30e31d3c6603c9b4ae6ba392de6a2327b658
evlrs <evlaurans@a112pc11L.local.isima.fr>
2021-06-17 09:59:25 +0200

    MAJ Site web

--- html/script.js
+++ html/script.js
@@ -1,12 +1,34 @@
-var btnPopup = document.getElementById('btnPopup');
-var overlay = document.getElementById('overlay');
-btnPopup.addEventListener('click',openMoadl);
-function openMoadl() {
-overlay.style.display='block';
+function openMoadl(event) {
+    var o = 
+    event.target.parentNode.getElementsByClassName("overlay")[0];
+    o.style.display='block';
+   // o.addEventListener('click',closeModal);
 }
 
-var btnClose = document.getElementById('btnClose');
-btnClose.addEventListener('click',closeModal);
-function closeModal() {
-overlay.style.display='none';
+function closeModal(event) {
+    var o = event.target;
+    console.log(o.className);
+    while (o.className !== "overlay") {
+        console.log(o.tagName);
+       o = o.parentNode;
+
+    }
+    o.style.display='none';
+}
+
+
+var btnPopup = document.getElementsByClassName('btnPopup');
+var overlay = document.getElementsByClassName('overlay');
+for(var i = 0; i < btnPopup.length; i++)
+{   
+    btnPopup[i].addEventListener('click',openMoadl);
+    //btnPopup[i].openMoadl();
+}
+
+
+var btnClose = document.getElementsByClassName('btnClose');
+var overlay = document.getElementsByClassName('overlay');
+for(var i = 0; i < btnClose.length; i++)
+{
+    btnClose[i].addEventListener('click',closeModal);  
 }
