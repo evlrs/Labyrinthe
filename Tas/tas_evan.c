b01bca75dc4b3062e2258ec32d087f34aa86d7b2
evlrs <evlaurans@a112pc11L.local.isima.fr>
2021-06-21 16:46:44 +0200

    Mise à jour du tas_evan début init tas

--- Tas/tas_evan.c
+++ Tas/tas_evan.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+
+void verifTas(int tas[], int taille);
+int parent(int indice);
+int droite(int indice);
+int gauche(int indice);
+void inserTas(int tas[],int taille, int val);
+int supprTas(int tas[],int taille);
+
+
 int taille=9;
 
 /*void initTas(int list[taille]){
@@ -9,62 +19,150 @@ int taille=9;
     while tas[taille]!=
 }*/
 
+/* floor*/
+
+
+void verifTas(int tas[], int taille){
+    int i=0;
+    int temp;
+    int petit;
+    int g;
+    int d;
+    while((tas[i]>tas[gauche(i)]||tas[i]>tas[droite(i)])&&(droite(i)<(taille-1))){
+        
+        g=gauche(i);
+        d=droite(i);
+        petit=i;
+        if (tas[i]>=tas[g] && gauche(i)<(taille-1)){
+            petit=g;
+            printf("a");           
+        }
+        if (tas[petit]>tas[d] && droite(i)<(taille-1) ){
+            petit=d;
+            printf("b");
+        }
+        if (petit!=i){
+            temp=tas[i];
+            tas[i]=tas[petit];
+            tas[petit]=temp;
+            i=petit;   // ici il y a l'implémentation
+        }
+    }
+        
+
+
+
+/*    while((tas[i]>tas[gauche(i)] || tas[i]>tas[droite(i)] && (arret!=0))){  
+        printf("a ");
+        if (gauche(i)>(taille-1)&&(droite(i)>(taille-1))){
+            printf("d");
+            arret=0;
+        }
+        if(tas[gauche(i)]<tas[i]){
+            printf("b ");
+
+            for(i=0;i<(taille+1);i++){
+            printf("%d ",tas[i]);
+            }
+            printf("\n ");
+            
+            temp=tas[gauche(i)];
+            tas[gauche(i)]=tas[i];
+            tas[i]=temp;
+            i=gauche(i);
+        }
+        if(tas[droite(i)]<tas[i]){
+            printf("c");
+
+            for(i=0;i<(taille+1);i++){
+            printf("%d ",tas[i]);
+            }
+            printf("\n ");
+            temp=tas[droite(i)];
+            tas[droite(i)]=tas[i];
+            tas[i]=temp;
+            i=droite(i);
+        }
+    }*/
+}
+
 
 int parent(int indice){
-    return floor((indice-1)/2);
+    return (indice-1)/2;
 }
 
-int droite(int indice){
-    return  2*indice-1;
-}
 
 int gauche(int indice){
     return 2*indice+1;
 }
 
-void inserTas(int tas[],int taille, int val){
-    taille+=1;
-    int i=taille;
-    int temp=0;
-    
-    while(val>tas[parent(i)]){
-
-        temp=tas[parent(i)];
-        tas[parent(i)]=val;
-        tas[i]=temp;
-        i=parent(i);
-    }
-
-   
-    
+int droite(int indice){
+    return  2*indice+2;
 }
 
 
-/*void supprTas(int tas[],int val){
-    int i=0;
-    while(tas[i]!=parent[indice]){
-        i+=1;
-    }
-    if (tas[i]=='NULL'){
-        tas[i-1]='NULL';
-        tas[i+1]='NULL';
-    }
 
-}*/
-
-
-
-
-void main(){
-    taille=9;
-    int tas[]={1,2,3,17,19,36,7,25,10};
-    int i=0;
-    inserTas(tas,taille,4);
+void inserTas(int tas[],int taille, int val){
+    taille+=1;
+    int i=taille-1;
+    int temp=0;
+    tas[i]=val;
     
-    //supprTas(tas[9]);
-    i=0;
-    for(i=0;i<(taille+1);i++){
+    while(tas[i]<tas[parent(i)]){
+        
+        temp=tas[parent(i)];
+        tas[parent(i)]=tas[i];
+        tas[i]=temp;
+        i=parent(i);
+
+    }
+    
+}
+
+/*on supprime la racine (la valeur en position 0)*/
+
+int supprTas(int tas[],int taille){
+    int temp=tas[0];
+    tas[0]=tas[taille-1];
+    //tas[taille-1]=temp;
+    verifTas(tas,taille);
+    taille=taille-1; 
+    printf("La valeur supprimée est %d \n ",temp);
+    return taille;
+
+}
+
+
+
+
+int main(){
+    taille=9;
+    int liste[]={1,2,3,17,19,36,7,25,100};
+    int i=0;
+    int tas[taille];
+    for(int i=0;i<taille;++i){
+        tas[i]=liste[i];
+    }
+    
+
+    for(i=0;i<(taille);i++){
         printf("%d ",tas[i]);
     }
     printf("\n");
+
+    inserTas(tas,taille,4);
+    for(i=0;i<(taille);i++){
+        printf("%d ",tas[i]);
+    }
+    printf("\n");
+
+    taille=supprTas(tas,taille);
+    i=0;
+    for(i=0;i<(taille);i++){
+        printf("%d ",tas[i]);
+    }
+    printf("\n");
+
+
+    return 0;
 }
